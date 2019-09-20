c1dfdb0c856877464778387346a75a4131fbaa5b
root <root@K.Mach>
2019-09-20 06:47:31 +0300

    little javascript validation added

--- assets/script/script2.js
+++ assets/script/script2.js
@@ -1,75 +1,29 @@
-// Variables or IDs of Username and Passwords Inputs:
-var fulllname = document.getElementById('fullname');
-var emailaddress = document.getElementById('emailaddress');
-var password = document.getElementById('password');
-var passwordC = document.getElementById('passwordC');
-/*----------------------------------------------------------------------------------*/
-// Variable or ID of submit Btn (Login Button which submits data to server).
-var submitBtn = document.getElementById('submitBtn');
-/*----------------------------------------------------------------------------------*/
-// Variables of html div elements which has class of 'input-div validate-input'.
-var allInputs = document.getElementsByClassName('input-div validate-input');
-var fullnameValidate = document.getElementsByClassName('input-div validate-input')[0];
-var emailaddressValidate = document.getElementsByClassName('input-div validate-input')[1];
-var passwordValidate = document.getElementsByClassName('input-div validate-input')[2];
-var passwordCValidate = document.getElementsByClassName('input-div validate-input')[3];
-/*----------------------------------------------------------------------------------*/
-// Variables of submitted emailaddress value and passwod value from inputs;
-var submittedFullname;
-var submittedEmailaddress;
-var submittedPassword;
-var submittedPaswordC;
-/*----------------------------------------------------------------------------------*/
+//we need to confirm the password
+var password=document.getElementById('password');
+var passwordc=document.getElementById('passwordC');
 
-function submitFunc(){
-    // If user entered blank spaces before or after in emailaddress or password inputs; Solution is trim();
-	submittedFullname = fullname.value.trim();
-    submittedEmailaddress = emailaddress.value.trim();
-    submittedPassword = password.value.trim();
-    
-    // If emailaddressaddress input was submitted blank then JS will apply the alert-validate style/class.
-    if(submittedFullname == ""){
-        fullnameValidate.className = 'input-div alert-validate';
-    };
-    
-    // If password input was submitted blank then JS will apply the alert-validate style/class.
-    if(submittedEmailaddress == ""){
-        emailaddressValidate.className = 'input-div alert-validate';
-    };
 
-    if(submittedPassword == ""){
-        passwordValidate.className = 'input-div alert-validate';
+function errorCheck(e)
+{
+    if (password.value == passwordc.value)
+    {
+        var len=password.value.length;
+        if (len<6)
+        {
+            e.preventDefault();
+            window.alert("password lenght should be >5");
+            password.focus(); //focus on the password text box
+            password.style.borderColor="red";
+            passwordc.style.borderColor="red";
+            return false;
+        }
     }
-    
-  /*  if(password.length == 8){
-    password.style.display = 'none';
-    console.log(password);
-	}*/
-
-};
-/*----------------------------------------------------------------------------------*/
-// The Log/Submit button event listener.
-submitBtn.onclick = submitFunc;
-	
-/*Below is the functionality of how alert will be gone when the user click any/current input.*/	
-// Focus:
-for(var i = 0; i < allInputs.length; i++){
-	allInputs[i].onclick = function(){
-		this.className = 'input-div validate-input';
-	};
-};
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-										// Sign Up Form JS 
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
+    else
+    { 
+        e.preventDefault();
+        window.alert("passwords do not match");
+        passwordc.focus(); //focus on the confirm password text box
+        passwordc.style.borderColor="red";
+        return false;
+    }
+}
